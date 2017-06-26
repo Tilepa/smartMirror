06ec27ae6a85039a61a2df28dea3aaf26d8e5b0e
Timon Sachweh <timon@sachweh.de>
2017-06-26 10:35:37 +0200

    Changed temperature sensor to work with temperature and humidity sensor

--- controller/teperatureSensor.py
+++ controller/teperatureSensor.py
@@ -1,20 +1,17 @@
-from model.configurations import temperature_id
+import os
+from sys import platform
+
+from model.configurations import pin_of_tempsensor, default_return_error
+
+if platform == "linux" or platform == "linux2":
+    import Adafruit_DHT
 
 
 def get_actual_temperature():
-    # 1-wire Slave Datei lesen
-    try:
-        file = open('/sys/bus/w1/devices/' + temperature_id + '/w1_slave')
-    except IOError:
-        print("There is no temperature sensor with the id: " + temperature_id)
-        return "No temperature"
-    file_content = file.read()
-    file.close()
-
-    # Temperaturwerte auslesen und konvertieren
-    string_value = file_content.split("\n")[1].split(" ")[9]
-    temperature = float(string_value[2:]) / 1000
-
-    # Temperatur ausgeben
-    return_value = '%6.2f' % temperature
-    return str(return_value)
+    if platform == "linux" or platform == "linux2":
+        # linux
+        sensor = Adafruit_DHT.DHT11
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_of_tempsensor)
+        return temperature + ", " + humidity
+    else:
+        return default_return_error
